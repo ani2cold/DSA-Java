bfa382d6cbd5a43a664187da37dddc5e20861bb4
Anirudh Nagar <ani2cold@gmail.com>
2026-07-18 22:33:39 +0530

    Time: 1 ms (99.9%), Space: 55.8 MB (18.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,24 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        
         int count=0;
-        int can=0;
+        int curr=0;
 
         for(int num:nums){
+
             if(count==0){
-                can=num;
+                curr=num;
             }
-            if(can==num){
+            if(curr==num){
                 count++;
             }else{
                 count--;
             }
         }
-        return can;
-        
+        return curr;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
