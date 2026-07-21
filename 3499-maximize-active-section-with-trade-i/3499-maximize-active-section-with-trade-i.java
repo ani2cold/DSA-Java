2688ec1194ac323c3e0e6a470c8d29723d795f5b
Anirudh Nagar <ani2cold@gmail.com>
2026-07-21 22:03:57 +0530

    Time: 67 ms (74.65%), Space: 47.7 MB (90.14%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
+
+        int n = s.length();
+
+        int ans = 0;
+        int i = 0;
+
+        int pre = Integer.MIN_VALUE;
+
+        int mx = 0;
+
+        while (i < n) {
+
+            int j = i + 1;
+
+            while (j < n && s.charAt(j) == s.charAt(i)) {
+                j++;
+            }
+
+            int len = j - i;
+
+            if (s.charAt(i) == '1') {
+
+                ans += len;
+
+            } else {
+
+                mx = Math.max(mx, pre + len);
+                pre = len;
+            }
+
+            i = j;
+        }
+
+        return ans + mx;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
