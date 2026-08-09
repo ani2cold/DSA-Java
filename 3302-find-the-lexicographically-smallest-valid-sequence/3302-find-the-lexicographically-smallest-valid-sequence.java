7b797b35690c936a29b3970a2f5171bef2ab6d07
Anirudh Nagar <ani2cold@gmail.com>
2026-08-09 09:48:13 +0530

    Time: 30 ms (97.37%), Space: 137.5 MB (47.37%) - LeetHub

---
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] validSequence(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[] ans = new int[m];
+        int[] last = new int[m];
+
+        Arrays.fill(last, -1);
+
+        int i = n - 1;
+        int j = m - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (word1.charAt(i) == word2.charAt(j)) {
+                last[j] = i;
+                j--;
+            }
+            i--;
+        }
+
+        boolean canChange = true;
+        j = 0;
+
+        for (i = 0; i < n && j < m; i++) {
+            if (word1.charAt(i) == word2.charAt(j)) {
+                ans[j] = i;
+                j++;
+            } else if (canChange &&
+                       (j == m - 1 || i < last[j + 1])) {
+                ans[j] = i;
+                j++;
+                canChange = false;
+            }
+        }
+
+        if (j != m) {
+            return new int[0];
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
