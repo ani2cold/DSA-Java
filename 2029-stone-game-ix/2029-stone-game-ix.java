ec585a911e9fb26bb7455b0b2f4ca9566eb60797
Anirudh Nagar <ani2cold@gmail.com>
2026-08-16 21:03:38 +0530

    Time: 3 ms (100%), Space: 112.8 MB (100%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+
+        int[] count = new int[3];
+
+        for (int stone : stones) {
+            count[stone % 3]++;
+        }
+
+        int c0 = count[0];
+        int c1 = count[1];
+        int c2 = count[2];
+
+        if (c0 % 2 == 0) {
+            return c1 > 0 && c2 > 0;
+        }
+
+        return Math.abs(c1 - c2) > 2;
+    }
+}
